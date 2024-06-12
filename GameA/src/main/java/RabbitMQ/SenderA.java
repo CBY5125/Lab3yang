2e42d514c7fc88fc328793eaf9bbe8be7bc26309
CBY5125 <cby5125@psu.edu>
2024-06-11 22:10:06 -0400

    Commit 3

--- GameA/src/main/java/RabbitMQ/SenderA.java
+++ GameA/src/main/java/RabbitMQ/SenderA.java
@@ -1,9 +1,26 @@
+/** Project: Systems Integration Space Game: File Files, RabbitMQ, and Web Service/JSON
+ * Purpose Details: use RabbitMQ and Web Services for sending and receiving game object data between applications GameA and GameB
+ * Course:IST242
+ * Author:Christina Yang
+ * Date Developed:Jun 8 2024
+ * Last Date Changed: Jun 11 2024
+ * Rev:4
+
+ */
 package RabbitMQ;
 
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
+/**
+ * RabbitMQ server will declares a queue, and sends a message to that queue.
+ * The SenderA send a message to the RabbitMQ queue named "SpaceGame".
+ * Create a connection and set server host
+ * Finds the queue named "SpaceGame"
+ * message is sent to the queue
+ * Print the confirmation message
+ */
 public class SenderA {
     private final static String QUEUE_NAME = "SpaceGame";
     public static void main(String[] argv) throws Exception {
--- GameB/src/main/java/RabbitMQ/ReciverB.java
+++ GameB/src/main/java/RabbitMQ/ReciverB.java
@@ -1,3 +1,12 @@
+/** Project: Systems Integration Space Game: File Files, RabbitMQ, and Web Service/JSON
+ * Purpose Details: use RabbitMQ and Web Services for sending and receiving game object data between applications GameA and GameB
+ * Course:IST242
+ * Author:Christina Yang
+ * Date Developed:Jun 8 2024
+ * Last Date Changed: Jun 11 2024
+ * Rev:4
+
+ */
 package RabbitMQ;
 
 import com.rabbitmq.client.Channel;
@@ -5,23 +14,32 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-public class ReciverB {
-        private final static String QUEUE_NAME = "SpaceGame";
+/**
+ * ReciverB class receives messages from a RabbitMQ queue
+ * "SpaceGame" is name of the RabbitMQ queue from which messages will be received.
+ * Create a connection, set the RabbitMQ server host
+ * Create a channel
+ * Declare a queue named
+ * Callback for messages deilvery
+ * print received then the message
+ */
+ public class ReciverB {
+    private final static String QUEUE_NAME = "SpaceGame";
 
-        public static void main(String[] argv) throws Exception {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                String message = new String(delivery.getBody(), "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
-            };
-            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 
-        }
     }
+}
